750b9a096d7958ea730bf0c1025ec86e411980ce
basiphobe <basiphobe@gmail.com>
2023-06-14 12:33:34 -0400

    Checkpoint

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,35 @@
 <script>
     import '../app.css'
+    import Nav from "../components/nav.svelte";
 </script>
 
-<slot />
-  
+<style>
+    #content {
+        text-align: center;
+        padding: 0 200px;
+        opacity: 0;
+        animation: fade-in 5s forwards;
+    }
+    
+    @keyframes fade-in {
+      100% {
+        opacity: 1;
+        filter: blur(0);
+      }
+    }
+</style>
+    
+<div id="contents">
+    <Nav />
+    <main class="masthead">
+        <div id="content">
+            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <div class="d-flex justify-content-center">
+                    <div class="text-center">
+                        <slot />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</div>
--- src/routes/components/main.svelte
+++
@@ -1,9 +0,0 @@
-<header class="masthead">
-    <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
-        <div class="d-flex justify-content-center">
-            <div class="text-center">
-                <h1 class="mx-auto my-0 text-uppercase">Home</h1>
-            </div>
-        </div>
-    </div>
-</header>
